cef93599c06fb0993ce3d381e37eaf8eac7d4432
CamilleM28 <Camillem2812@gmail.com>
2022-01-12 21:13:36 +0000

    Session 3 - Completed tasks

--- session03/task01/src/index.js
+++ session03/task01/src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const name = '[YOUR NAME GOES HERE]';
+const name = "Camille";
 
-function Profile(name){
-  //  if(){
-    return <main id="content" role="main" class="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
-            </main>
-  //  }
-  //  else {
-  //    return <main id="content" role="main" class="base">
-  //      <h1>Just a React Page</h1>
-  //      <p>Nothing to see here!</p>
-  //    </main>  
-  //  }
+function Profile(props) {
+  if (name.toLowerCase() === "camille") {
+    return (
+      <main id="content" role="main" class="base">
+        <h1>{name}'s React Page</h1>
+        <p>I am from Birmingham</p>
+      </main>
+    );
+  } else {
+    return (
+      <main id="content" role="main" class="base">
+        <h1>Just a React Page</h1>
+        <p>Nothing to see here!</p>
+      </main>
+    );
+  }
 }
 
-ReactDOM.render(Profile(),document.getElementById('root'));
+ReactDOM.render(Profile(), document.getElementById("root"));

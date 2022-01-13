b88d853cf24022d195dfd8829e95f5611b053f6f
CamilleM28 <Camillem2812@gmail.com>
2022-01-13 00:26:22 +0000

    session 3 - tasks 1 and 2 completed

--- session03/task01/src/index.js
+++ session03/task01/src/index.js
@@ -6,14 +6,14 @@ const name = "Camille";
 function Profile(props) {
   if (name.toLowerCase() === "camille") {
     return (
-      <main id="content" role="main" class="base">
+      <main id="content" role="main" className="base">
         <h1>{name}'s React Page</h1>
         <p>I am from Birmingham</p>
       </main>
     );
   } else {
     return (
-      <main id="content" role="main" class="base">
+      <main id="content" role="main" className="base">
         <h1>Just a React Page</h1>
         <p>Nothing to see here!</p>
       </main>
--- session03/task02/src/index.js
+++ session03/task02/src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Profile from './Profile';
+import React from "react";
+import ReactDOM from "react-dom";
+import Profile from "./Profile";
 
 function App() {
-  return (
-      <Profile name="[YOUR NAME]" bio="[YOUR SHORT BIO GOES HERE]" />
-  );
+  return <Profile name="Camille" bio="I am from birmingham" />;
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
